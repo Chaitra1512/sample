b50c885ba91e86afb6c3cde6fef94e8718aee26a
Chaitra1512 <chaitra152002@gmail.com>
2024-03-19 16:23:42 +0530

    2.0

---
+++ hello.h
@@ -0,0 +1,10 @@
+
+void swap(int *a,int *b)
+{
+
+    int c;
+    c=*a;
+    *a=*b;
+    *b=c;
+
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"hello.h"
+
+int main()
+{
+int a=10,b=5;
+    swap(&a,&b);
+    printf("after swapping : a=%d b=%d",a,b);
+
+}
+
